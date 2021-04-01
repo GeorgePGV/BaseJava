db5586a694f0a689e1668b970d2bb4177a1c0542
GeorgePGV <pogosian.jor@yandex.ru>
2021-04-01 17:30:17 +0300

    Почти сделал SortedArray

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -15,15 +15,20 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     public void save(Resume resume) {
-        sortInOrder(resume, storage);
+      //  Comparator<Resume> comparator = (Resume1, Resume2) -> Resume1.getUuid().compareTo(Resume2.getUuid());
+        int idx = Arrays.binarySearch(storage,0,size,resume);
+        if (idx < 0) {
+            idx = -idx - 1;
+        }
+        for (int i = idx + 1; i < size; i++) {
+            storage[i] = storage[i - 1];
+        }
+        storage[idx] = resume;
         size++;
     }
 
-    public static void sortInOrder(Resume x, Resume[] storage) {
-        if (x == null)
-            throw new IllegalArgumentException();
-
-        int idx = Arrays.binarySearch(storage, 0, size , x.getUuid());
+    public static void sortInOrder(String uuid, Resume[] storage) {
+        int idx = Arrays.binarySearch(storage, 0, size ,uuid);
 
         if (idx < 0) {
             idx = -idx - 1;
@@ -32,7 +37,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
         for (int i = idx + 1; i < size - 1; i++) {
             storage[i] = storage[i - 1];
         }
-        storage[idx] = x;
+        //storage[idx] = ;
     }
 }
 
