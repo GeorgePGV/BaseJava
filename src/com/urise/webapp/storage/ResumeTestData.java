01930327b9c6bdb6ac5d83fb708890d0c9f5104f
GeorgePGV <pogosian.jor@yandex.ru>
2021-05-20 17:39:24 +0300

    Набросок ResumeTestData

--- src/com/urise/webapp/storage/ResumeTestData.java
+++ src/com/urise/webapp/storage/ResumeTestData.java
@@ -1,7 +1,49 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.model.SectionWithList;
+import com.urise.webapp.model.SectionWithText;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResumeTestData {
     public static void main(String[] args){
-
+        Resume resume = new Resume("uuid1", "Григорий Кислин");
+        SectionWithText personalQualities = new SectionWithText("Аналитический склад ума, сильная логика, креативность, инициативность. Пурист кода и архитектуры.");
+        SectionWithText position = new SectionWithText("Ведущий стажировок и корпоративного обучения по Java Web и Enterprise технологиям");
+        String achievement1 = "С 2013 года: разработка проектов \"Разработка Web приложения\",\"Java Enterprise\", \"Многомодульный maven. Многопоточность. XML (JAXB/StAX). Веб сервисы (JAX-RS/SOAP). Удаленное взаимодействие (JMS/AKKA)\". Организация онлайн стажировок и ведение проектов. Более 1000 выпускников.";
+        String achievement2 = "Реализация двухфакторной аутентификации для онлайн платформы управления проектами Wrike. Интеграция с Twilio, DuoSecurity, Google Authenticator, Jira, Zendesk.";
+        String achievement3 = "Налаживание процесса разработки и непрерывной интеграции ERP системы River BPM. Интеграция с 1С, Bonita BPM, CMIS, LDAP. Разработка приложения управления окружением на стеке: Scala/Play/Anorm/JQuery. Разработка SSO аутентификации и авторизации различных ERP модулей, интеграция CIFS/SMB java сервера.";
+        String achievement4 = "Реализация c нуля Rich Internet Application приложения на стеке технологий JPA, Spring, Spring-MVC, GWT, ExtGWT (GXT), Commet, HTML5, Highstock для алгоритмического трейдинга.";
+        String achievement5 = "Создание JavaEE фреймворка для отказоустойчивого взаимодействия слабо-связанных сервисов (SOA-base архитектура, JAX-WS, JMS, AS Glassfish). Сбор статистики сервисов и информации о состоянии через систему мониторинга Nagios. Реализация онлайн клиента для администрирования и мониторинга системы по JMX (Jython/ Django).";
+        String achievement6 = "Реализация протоколов по приему платежей всех основных платежных системы России (Cyberplat, Eport, Chronopay, Сбербанк), Белоруcсии(Erip, Osmp) и Никарагуа.";
+        List<String> achievement = new ArrayList();
+        for (int i = 0; i < 6; i++){
+            achievement.add(achievement + "i");
+        }
+        SectionWithList achievements = new SectionWithList(achievement);
+        String qualification1 = "JEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2";
+        String qualification2 = "Version control: Subversion, Git, Mercury, ClearCase, Perforce";
+        String qualification3 = "DB: PostgreSQL(наследование, pgplsql, PL/Python), Redis (Jedis), H2, Oracle,";
+        String qualification4 = "MySQL, SQLite, MS SQL, HSQLDB";
+        String qualification5 = "Languages: Java, Scala, Python/Jython/PL-Python, JavaScript, Groovy,";
+        String qualification6 = "XML/XSD/XSLT, SQL, C/C++, Unix shell scripts,";
+        String qualification7 = "Java Frameworks: Java 8 (Time API, Streams), Guava, Java Executor, MyBatis, Spring (MVC, Security, Data, Clouds, Boot), JPA (Hibernate, EclipseLink), Guice, GWT(SmartGWT, ExtGWT/GXT), Vaadin, Jasperreports, Apache Commons, Eclipse SWT, JUnit, Selenium (htmlelements).";
+        String qualification8 = "Python: Django";
+        String qualification9 = "JavaScript: jQuery, ExtJS, Bootstrap.js, underscore.js";
+        String qualification10 = "Scala: SBT, Play2, Specs2, Anorm, Spray, Akka";
+        String qualification11 = "Технологии: Servlet, JSP/JSTL, JAX-WS, REST, EJB, RMI, JMS, JavaMail, JAXB, StAX, SAX, DOM, XSLT, MDB, JMX, JDBC, JPA, JNDI, JAAS, SOAP, AJAX, Commet, HTML5, ESB, CMIS, BPMN2, LDAP, OAuth1, OAuth2, JWT.";
+        String qualification12 = "Инструменты: Maven + plugin development, Gradle, настройка Ngnix,";
+        String qualification13 = "администрирование Hudson/Jenkins, Ant + custom task, SoapUI, JPublisher, Flyway, Nagios, iReport, OpenCmis, Bonita, pgBouncer.";
+        String qualification14 = "Отличное знание и опыт применения концепций ООП, SOA, шаблонов проектрирования, архитектурных шаблонов, UML, функционального программирования";
+        String qualification15 = "Родной русский, английский \"upper intermediate\"";
+        List<String> qualification = new ArrayList();
+        for (int i = 0; i < 6; i++){
+            qualification.add(qualification + "i");
+        }
+        SectionWithList qualifications = new SectionWithList(qualification);
+        System.out.println(personalQualities.getContent());
+        System.out.println(position.getContent());
     }
 }
