c5be9090a0d4f164262c7c5309578e6dd0697508
GeorgePGV <pogosian.jor@yandex.ru>
2021-06-01 17:47:28 +0300

    Исправил

--- src/com/urise/webapp/ResumeTestData.java
+++ src/com/urise/webapp/ResumeTestData.java
@@ -15,7 +15,7 @@ public class ResumeTestData {
 
         SingleLineSection position = new SingleLineSection("Ведущий стажировок и корпоративного обучения по Java Web и Enterprise технологиям");
 
-        List<String> achievement = new ArrayList<String>();
+        List<String> achievement = new ArrayList<>();
         achievement.add("С 2013 года: разработка проектов \"Разработка Web приложения\",\"Java Enterprise\", \"Многомодульный maven. Многопоточность. XML (JAXB/StAX). Веб сервисы (JAX-RS/SOAP). Удаленное взаимодействие (JMS/AKKA)\\\". Организация онлайн стажировок и ведение проектов. Более 1000 выпускников.");
         achievement.add("Реализация двухфакторной аутентификации для онлайн платформы управления проектами Wrike. Интеграция с Twilio, DuoSecurity, Google Authenticator, Jira, Zendesk.");
         achievement.add("Налаживание процесса разработки и непрерывной интеграции ERP системы River BPM. Интеграция с 1С, Bonita BPM, CMIS, LDAP. Разработка приложения управления окружением на стеке: Scala/Play/Anorm/JQuery. Разработка SSO аутентификации и авторизации различных ERP модулей, интеграция CIFS/SMB java сервера.");
@@ -24,7 +24,7 @@ public class ResumeTestData {
         achievement.add("Реализация протоколов по приему платежей всех основных платежных системы России (Cyberplat, Eport, Chronopay, Сбербанк), Белоруcсии(Erip, Osmp) и Никарагуа.");
         BulletedListSection achievements = new BulletedListSection(achievement);
 
-        List<String> qualification = new ArrayList<String>();
+        List<String> qualification = new ArrayList<>();
         qualification.add("JEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2");
         qualification.add("Version control: Subversion, Git, Mercury, ClearCase, Perforce");
         qualification.add("DB: PostgreSQL(наследование, pgplsql, PL/Python), Redis (Jedis), H2, Oracle,");
@@ -42,7 +42,7 @@ public class ResumeTestData {
         qualification.add("Родной русский, английский \"upper intermediate\"");
         BulletedListSection qualifications = new BulletedListSection(qualification);
 
-        List<Position> organizations = new ArrayList<Position>();
+        List<Position> organizations = new ArrayList<>();
         organizations.add(new Position("Java Online Projects","https://javaops.ru/","Автор проекта.","Создание, организация и проведение Java онлайн проектов и стажировок", LocalDate.of(2013, Month.OCTOBER, 1), LocalDate.of(2021, Month.MAY, 1)));
         organizations.add(new Position("Wrike", "https://www.wrike.com/", "Старший разработчик (backend)", "Проектирование и разработка онлайн платформы управления проектами Wrike (Java 8 API, Maven, Spring, MyBatis, Guava, Vaadin, PostgreSQL, Redis). Двухфакторная аутентификация, авторизация по OAuth1, OAuth2, JWT SSO." , LocalDate.of(2014, Month.OCTOBER, 1), LocalDate.of(2017, Month.JANUARY, 1)));
         organizations.add(new Position("Luxoft (Deutsche Bank)", "https://career.luxoft.com/locations/russia/", "Ведущий программист", "Участие в проекте Deutsche Bank CRM (WebLogic, Hibernate, Spring, Spring MVC, SmartGWT, GWT, Jasper, Oracle). Реализация клиентской и серверной части CRM. Реализация RIA-приложения для администрирования, мониторинга и анализа результатов в области алгоритмического трейдинга. JPA, Spring, Spring-MVC, GWT, ExtGWT (GXT), Highstock, Commet, HTML5." , LocalDate.of(2010, Month.DECEMBER, 1), LocalDate.of(2012, Month.APRIL, 1)));
@@ -52,7 +52,7 @@ public class ResumeTestData {
         organizations.add(new Position("Alcatel" , "http://www.alcatel.ru/","Инженер по аппаратному и программному тестированию", "Тестирование, отладка, внедрение ПО цифровой телефонной станции Alcatel 1000 S12 (CHILL, ASM).", LocalDate.of(1997, Month.SEPTEMBER, 1), LocalDate.of(2005, Month.JANUARY, 1)));
         Organization organization = new Organization(organizations);
 
-        List<Position> education = new ArrayList<Position>();
+        List<Position> education = new ArrayList<>();
         education.add(new Position("Coursera", "https://www.coursera.org/learn/progfun1" , "Student", "\"Functional Programming Principles in Scala\" by Martin Odersky", LocalDate.of(2013, Month.MARCH, 1), LocalDate.of(2013, Month.MAY, 1)));
         education.add(new Position("Luxoft", "https://www.luxoft-training.ru/kurs/obektno-orientirovannyy_analiz_i_proektirovanie_na_uml.html", "Student", "Курс \"Объектно-ориентированный анализ ИС. Концептуальное моделирование на UML.\"", LocalDate.of(2011, Month.MARCH, 1), LocalDate.of(2011, Month.APRIL, 1)));
         education.add(new Position("Siemens AG", "https://new.siemens.com/ru/ru.html", "Student", "3 месяца обучения мобильным IN сетям (Берлин)", LocalDate.of(2005, Month.JANUARY, 1), LocalDate.of(2005, Month.APRIL, 1)));
