8ef3f8c9edaa26f606788f3b31d347ce3a7cc82a
GeorgePGV <pogosian.jor@yandex.ru>
2021-08-02 20:23:54 +0300

    Всё исправил

--- src/com/urise/webapp/MainStream.java
+++ src/com/urise/webapp/MainStream.java
@@ -1,30 +1,24 @@
 package com.urise.webapp;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.partitioningBy;
 
 public class MainStream {
+    public static void main(String[] args) throws InterruptedException {
+        int[] mas = {5, 1, 1, 1, 2, 2, 2, 3, 3, 5};
+        List<Integer> list = Arrays.stream(mas).boxed().collect(Collectors.toList());
+        System.out.println(minValue(mas));
+        System.out.println(oddOrEven(list));
+    }
+
     private static int minValue(int[] values) {
-        int identity = 0;
-        return Arrays.stream(values).boxed().distinct().sorted().reduce(identity ,(x, y) -> x * 10 + y);
+        return Arrays.stream(values).distinct().sorted().reduce(0, (x, y) -> x * 10 + y);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
         return integers.stream().collect(partitioningBy(x -> x % 2 == 0)).get(integers.stream().collect(partitioningBy(x -> x % 2 == 0)).get(false).size() % 2 != 0);
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        int[] mas = { 5,1,1,1,2,2,2,3,3,5 };
-        List<Integer> list = new ArrayList();
-        list.add(2);
-        list.add(6);
-        list.add(8);
-        list.add(3);
-        list.add(1);
-        System.out.println(minValue(mas));
-        System.out.println(oddOrEven(list));
     }
 }
